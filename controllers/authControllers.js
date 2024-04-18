76f4efa65644bc380ff5318b76d406a31a2b8778
phucquan102 <ngophucquan2002@gmail.com>
2024-04-19 00:47:20 +0700

    up

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,7 +2,6 @@ const User = require("../models/UserModel");
 const bcrypt = require("bcrypt");
 
 const authController = {
-
     registerUser: async (req, res) => {
         try {
             const salt = await bcrypt.genSalt(10);  
